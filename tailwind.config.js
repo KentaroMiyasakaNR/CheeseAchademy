26daa4ee32c171d6a405e8d2a6bd28bcee550988
DESKTOP-SU0IT7T\KentaroMiyasaka <kentamiya7426@gmail.com>
2024-04-24 22:38:21 +0900

    とりあえず最低限のページ作成

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./html_miyasaka_28/index.html",
+    './js/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
